fe2b5d92176509096417b1bc972972319611aab1
YiYing He <yiying@secondstate.io>
2020-06-23 17:24:29 +0800

    [Host] Use memory instance pointer instead of reference in host function to accept nullptr. nullptr checking also added.

--- include/storage_func.h
+++ include/storage_func.h
@@ -13,57 +13,57 @@ namespace Host {
 class SSVMStorageCreateUUID : public SSVMStorage<SSVMStorageCreateUUID> {
 public:
   SSVMStorageCreateUUID(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<uint32_t> body(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<uint32_t> body(Runtime::Instance::MemoryInstance *MemInst);
 };
 
 class SSVMStorageBeginStoreTx : public SSVMStorage<SSVMStorageBeginStoreTx> {
 public:
   SSVMStorageBeginStoreTx(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst,
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst,
                     uint32_t NewKey);
 };
 
 class SSVMStorageBeginLoadTx : public SSVMStorage<SSVMStorageBeginLoadTx> {
 public:
   SSVMStorageBeginLoadTx(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst,
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst,
                     uint32_t NewKey);
 };
 
 class SSVMStorageStoreI32 : public SSVMStorage<SSVMStorageStoreI32> {
 public:
   SSVMStorageStoreI32(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst, uint32_t Value);
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst, uint32_t Value);
 };
 
 class SSVMStorageLoadI32 : public SSVMStorage<SSVMStorageLoadI32> {
 public:
   SSVMStorageLoadI32(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<uint32_t> body(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<uint32_t> body(Runtime::Instance::MemoryInstance *MemInst);
 };
 
 class SSVMStorageStoreI64 : public SSVMStorage<SSVMStorageStoreI64> {
 public:
   SSVMStorageStoreI64(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst, uint64_t Value);
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst, uint64_t Value);
 };
 
 class SSVMStorageLoadI64 : public SSVMStorage<SSVMStorageLoadI64> {
 public:
   SSVMStorageLoadI64(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<uint64_t> body(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<uint64_t> body(Runtime::Instance::MemoryInstance *MemInst);
 };
 
 class SSVMStorageEndStoreTx : public SSVMStorage<SSVMStorageEndStoreTx> {
 public:
   SSVMStorageEndStoreTx(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst);
 };
 
 class SSVMStorageEndLoadTx : public SSVMStorage<SSVMStorageEndLoadTx> {
 public:
   SSVMStorageEndLoadTx(StorageEnvironment &Env) : SSVMStorage(Env) {}
-  Expect<void> body(Runtime::Instance::MemoryInstance &MemInst);
+  Expect<void> body(Runtime::Instance::MemoryInstance *MemInst);
 };
 
 } // namespace Host
